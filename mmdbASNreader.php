ea7171de60d13033c43a6c8f087dec2d8f658d3a
matomoto <35422079+matomoto@users.noreply.github.com>
2026-07-08 17:14:46 +0200

    Update mmdbASNreader.php

--- mmdbASNreader.php
+++ mmdbASNreader.php
@@ -1,19 +1,10 @@
 <?php
 
-require_once __DIR__ . '/mmdbASNarray.php';
+require __DIR__ . '/mmdbASNarray.php';
 
 $mmdb_visitor_ip = $_SERVER['REMOTE_ADDR'];
-// $mmdb_visitor_ip = '8.8.8.8'; // "Google LLC"
 
-require_once __DIR__ . '/composer/vendor/autoload.php';
-
-// if (class_exists(\MaxMind\Db\Reader::class)) {
-    // echo "MaxMind DB Reader is available.";
-	// echo PHP_EOL;
-// } else {
-    // echo "MaxMind DB Reader is unavailable.";
-	// echo PHP_EOL;
-// }
+require __DIR__ . '/composer/vendor/autoload.php';
 
 use MaxMind\Db\Reader;
 
@@ -28,10 +19,17 @@ function mmdb_scandir_function($mmdb_scandir_path) {
 	unset($mmdb_scandir_array[array_search(".", $mmdb_scandir_array, true)]);
 	unset($mmdb_scandir_array[array_search("..", $mmdb_scandir_array, true)]);
 	$mmdb_scandir_array_clean = array_values($mmdb_scandir_array);
-			
-	sort($mmdb_scandir_array_clean, SORT_NATURAL | SORT_FLAG_CASE);
 	
-	return $mmdb_scandir_array_clean;
+	for ($i = 0; $i < count($mmdb_scandir_array_clean); $i++) {
+		if (pathinfo($mmdb_scandir_array_clean[$i], PATHINFO_EXTENSION) != 'mmdb') {
+			unset($mmdb_scandir_array_clean[$i]);
+		}
+	}
+	$mmdb_scandir_array_clean_clean = array_values($mmdb_scandir_array_clean);
+	
+	sort($mmdb_scandir_array_clean_clean, SORT_NATURAL | SORT_FLAG_CASE);
+	
+	return $mmdb_scandir_array_clean_clean;
 }
 
 $mmdb_scandir_path_full = __DIR__ . '/database';
@@ -42,7 +40,7 @@ $mmdb_reader = new Reader(__DIR__ . '/database/' . $mmdb_scandir_filename);
 
 $mmdb_record = $mmdb_reader->get($mmdb_visitor_ip);
 
-// var_dump($record);
+// var_dump($mmdb_record);
 /*
 array(2) {
   ["autonomous_system_number"]=>
@@ -58,7 +56,6 @@ foreach ($mmdb_ASN_array as $mmdb_asn_name) {
 	if (str_contains(strtolower($mmdb_record["autonomous_system_organization"]), strtolower($mmdb_asn_name)) == true) {
 		// 451 - Unavailable For Legal Reasons (Copyright, etc.)
 		http_response_code(451);
-		// var_dump(http_response_code());
-		exit;
+		exit(0);
 	}
 }
